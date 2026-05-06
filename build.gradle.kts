396d9e36f8dcc546da0230afc314306bcff28674
Amstrong <amstrongrayan219@gmail.com>
2026-05-06 17:57:01 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,4 @@ allprojects {
         google()
         mavenCentral()
     }
-}    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
